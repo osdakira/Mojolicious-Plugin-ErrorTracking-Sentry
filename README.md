b7f6f1974d1610a8ada6eb7edd570b06a785487a
Akira Osada <osd.akira@gmail.com>
2017-12-12 12:13:20 +0900

    ce minil new Mojolicious::Plugin::ErrorTracking::Sentry

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Mojolicious::Plugin::ErrorTracking::Sentry - It's new $module
+
+# SYNOPSIS
+
+    use Mojolicious::Plugin::ErrorTracking::Sentry;
+
+# DESCRIPTION
+
+Mojolicious::Plugin::ErrorTracking::Sentry is ...
+
+# LICENSE
+
+Copyright (C) Akira Osada.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Akira Osada <osd.akira@gmail.com>
---
+++ lib/Mojolicious/Plugin/ErrorTracking/Sentry.pm
@@ -0,0 +1,39 @@
+package Mojolicious::Plugin::ErrorTracking::Sentry;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Mojolicious::Plugin::ErrorTracking::Sentry - It's new $module
+
+=head1 SYNOPSIS
+
+    use Mojolicious::Plugin::ErrorTracking::Sentry;
+
+=head1 DESCRIPTION
+
+Mojolicious::Plugin::ErrorTracking::Sentry is ...
+
+=head1 LICENSE
+
+Copyright (C) Akira Osada.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Akira Osada E<lt>osd.akira@gmail.comE<gt>
+
+=cut
+
